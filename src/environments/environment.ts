f432c636881fa5b77b90888da652d2ec5389a30e
miguelhem <miguelhm2394@gmail.com>
2023-08-10 03:04:22 -0600

    firebase hosting fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
+    projectId: 'minesweep-angular',
+    appId: '1:892391961566:web:d61a467dd41c7aee50234f',
+    storageBucket: 'minesweep-angular.appspot.com',
     apiKey: 'AIzaSyAp7608JT_atIqWQTFbhYE2M7qhePqX0UY',
     authDomain: 'minesweep-angular.firebaseapp.com',
-    projectId: 'minesweep-angular',
-    storageBucket: 'minesweep-angular.appspot.com',
     messagingSenderId: '892391961566',
-    appId: '1:892391961566:web:d61a467dd41c7aee50234f',
   },
 };
